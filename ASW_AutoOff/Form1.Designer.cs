7cf74294707e3c294423279a8409ac0c78ebf4f6
LABO405-PC <LABO405-PC@LABO405-PC>
2025-12-28 15:40:54 +0900

    プロジェクト ファイルを追加します。

---
+++ ASW_AutoOff/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ASW_AutoOff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.AutoStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitApp = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "MetaLinkのスムーズ機能を自動で無効化します";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AutoStart,
+            this.ExitApp});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 48);
+            // 
+            // AutoStart
+            // 
+            this.AutoStart.CheckOnClick = true;
+            this.AutoStart.Name = "AutoStart";
+            this.AutoStart.Size = new System.Drawing.Size(122, 22);
+            this.AutoStart.Text = "自動起動";
+            this.AutoStart.Click += new System.EventHandler(this.autoStartToolStripMenuItem_Click);
+            // 
+            // ExitApp
+            // 
+            this.ExitApp.Name = "ExitApp";
+            this.ExitApp.Size = new System.Drawing.Size(122, 22);
+            this.ExitApp.Text = "終了";
+            this.ExitApp.Click += new System.EventHandler(this.ExitApp_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem AutoStart;
+        private System.Windows.Forms.ToolStripMenuItem ExitApp;
+    }
+}
+
---
+++ ASW_AutoOff/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace ASW_AutoOff
+{
+    public partial class Form1 : Form
+    {
+        private bool isDetected = false;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            string runKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(runKey, false))
+            {
+                if (key.GetValue("ASW_AutoOff") != null)
+                {
+                    AutoStart.Checked = true;
+                }
+            }
+        }
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("OculusDash");
+
+            if (processes.Length > 0 && !isDetected)
+            {
+                isDetected = true;
+
+                await System.Threading.Tasks.Task.Delay(10000);
+
+                SendCtrlNum1();
+
+                notifyIcon1.ShowBalloonTip(3000, "ASW_AutoOff", "スムーズ機能無効化のために Ctrl+Num1 のコマンドを送信しました", ToolTipIcon.Info);
+            }
+            else if (processes.Length == 0)
+            {
+                isDetected = false;
+            }
+        }
+
+        private void SendCtrlNum1()
+        {
+            const byte VK_CONTROL = 0x11;
+            const byte VK_NUMPAD1 = 0x61;
+            const uint KEYEVENTF_KEYUP = 0x0002;
+
+            keybd_event(VK_CONTROL, 0, 0, 0);
+            keybd_event(VK_NUMPAD1, 0, 0, 0);
+            keybd_event(VK_NUMPAD1, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+        }
+        private void autoStartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string runKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            string appName = "ASW_AutoOff";
+            string appPath = Application.ExecutablePath;
+
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(runKey, true))
+                {
+                    if (AutoStart.Checked)
+                    {
+                        key.SetValue(appName, "\"" + appPath + "\"");
+                        MessageBox.Show("スタートアップに登録しました。\rPC起動時に自動で起動します。", "スタートアップ登録");
+                    }
+                    else
+                    {
+                        key.DeleteValue(appName, false);
+                        MessageBox.Show("スタートアップから解除しました。", "スタートアップ解除");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("設定の変更に失敗しました: " + ex.Message);
+            }
+        }
+
+        private void ExitApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
