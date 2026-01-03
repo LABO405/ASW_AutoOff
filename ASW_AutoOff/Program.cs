b630848fde4fda826186e6049d3fee1ff210591b
LABO405 <reddosuton3@gmail.com>
2026-01-04 06:16:18 +0900

    リソースのゴミ等を撤去

--- ASW_AutoOff/Program.cs
+++ ASW_AutoOff/Program.cs
@@ -1,21 +1,18 @@
 ﻿using System;
-using System.Threading; // これが必要
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ASW_AutoOff
 {
     static class Program
     {
-        // アプリ固有の識別名（適当な英数字の羅列でOKです）
         private static Mutex mutex = new Mutex(false, "ASW_AutoOff_Unique_Mutex_Name");
 
         [STAThread]
         static void Main()
         {
-            // Mutexの所有権を試みる
             if (!mutex.WaitOne(0, false))
             {
-                // すでに起動している場合はメッセージを出して終了
                 MessageBox.Show("ASW_AutoOff は既に起動しています。", "多重起動を停止しました。", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
@@ -24,7 +21,6 @@ namespace ASW_AutoOff
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            // 終了時にMutexを解放
             GC.KeepAlive(mutex);
             mutex.ReleaseMutex();
         }
